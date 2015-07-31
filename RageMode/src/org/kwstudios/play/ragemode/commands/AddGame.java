ae66e6da914ed1ab86891b32d3595fde48921d93
Simon <simon.artner@gmail.com>
2015-07-31 17:56:39 +0200

    Added functionality to AddGame

--- RageMode/src/org/kwstudios/play/ragemode/commands/AddGame.java
+++ RageMode/src/org/kwstudios/play/ragemode/commands/AddGame.java
@@ -1,8 +1,10 @@
 package org.kwstudios.play.ragemode.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.kwstudios.play.ragemode.toolbox.ConfigFactory;
 
 public class AddGame {
 	
@@ -17,6 +19,29 @@ public class AddGame {
 		this.label = label;
 		this.args = args;
 		this.fileConfiguration = fileConfiguration;
+		
+		addGametoConfig();
 	}
+
+	private void addGametoConfig(){
+		int x = Integer.parseInt(args[2]);
+		try {
+		}	catch (NumberFormatException i) {
+				player.sendMessage(ChatColor.DARK_RED + args[2]+" is not a number.");
+				return;
+		}
+		
+		if(fileConfiguration.get("settings.games."+args[1])=="") {
+			player.sendMessage(ChatColor.DARK_RED + args[1] + " already exists.");
+			return;
+		}
+			
+		ConfigFactory.setString("settings.games", args[1], "", fileConfiguration);
+		
+		ConfigFactory.setInt("settings.games." + args[1], "maxplayers", Integer.parseInt(args[2]), fileConfiguration);
+		
+		ConfigFactory.setString("settings.games." + args[1], "world", player.getWorld().getName(), fileConfiguration);
+		return;
+	} 
 
 }

5f4d1ff995ba138d3d0a003b729176953045c0c2
Y-brin <koray@koska.at>
2015-07-31 23:38:02 +0200

    Configured MapChecker, now it should work.

--- src/org/kwstudios/play/ragemode/toolbox/ConfigFactory.java
+++ src/org/kwstudios/play/ragemode/toolbox/ConfigFactory.java
@@ -6,17 +6,17 @@ import org.bukkit.configuration.file.FileConfiguration;
 
 public class ConfigFactory {
 
-//	private static final File CONFIG_DIRECTORY = new File("KWPlugin");
-//	private static final File CONFIG_FILE = new File("KWPlugin/config.yml");
-//	private static YamlConfiguration config = null;
+	// private static final File CONFIG_DIRECTORY = new File("KWPlugin");
+	// private static final File CONFIG_FILE = new File("KWPlugin/config.yml");
+	// private static YamlConfiguration config = null;
 
-//	public static YamlConfiguration getConfig() {
-//		createConfigFile();
-//		if (config == null) {
-//			config = YamlConfiguration.loadConfiguration(configFile);
-//		}
-//		return config;
-//	}
+	// public static YamlConfiguration getConfig() {
+	// createConfigFile();
+	// if (config == null) {
+	// config = YamlConfiguration.loadConfiguration(configFile);
+	// }
+	// return config;
+	// }
 
 	public static Boolean getBoolean(String path, String key, FileConfiguration fileConfiguration) {
 		return fileConfiguration.getBoolean(path + "." + key);
@@ -24,62 +24,70 @@ public class ConfigFactory {
 
 	public static void setBoolean(String path, String key, Boolean value, FileConfiguration fileConfiguration) {
 		fileConfiguration.set(path + "." + key, value);
-//		save();
+		// save();
 	}
-	
+
 	public static String getString(String path, String key, FileConfiguration fileConfiguration) {
 		return fileConfiguration.getString(path + "." + key);
 	}
 
 	public static void setString(String path, String key, String value, FileConfiguration fileConfiguration) {
 		fileConfiguration.set(path + "." + key, value);
-//		save();
+		// save();
 	}
-	
+
 	public static int getInt(String path, String key, FileConfiguration fileConfiguration) {
-		return fileConfiguration.getInt(path + "." + key);
+		try {
+			return fileConfiguration.getInt(path + "." + key);
+		} catch (Exception e) {
+			return -32500000;
+		}
 	}
 
 	public static void setInt(String path, String key, int value, FileConfiguration fileConfiguration) {
 		fileConfiguration.set(path + "." + key, value);
-//		save();
+		// save();
 	}
-	
+
 	public static double getDouble(String path, String key, FileConfiguration fileConfiguration) {
+		try{
 		return fileConfiguration.getDouble(path + "." + key);
+		}catch(Exception e){
+			return -32500000;
+		}
 	}
 
 	public static void setDouble(String path, String key, double value, FileConfiguration fileConfiguration) {
 		fileConfiguration.set(path + "." + key, value);
-//		save();
+		// save();
 	}
-	
-	public static Set<String> getKeysUnderPath(String path, boolean deep, FileConfiguration fileConfiguration){
+
+	public static Set<String> getKeysUnderPath(String path, boolean deep, FileConfiguration fileConfiguration) {
 		Set<String> keys = fileConfiguration.getConfigurationSection(path).getKeys(deep);
 		return keys;
 	}
 
-//	private static void save(FileConfiguration fileConfiguration) {
-//		createConfigFile();
-//		try {
-//			fileConfiguration.sa;
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	// private static void save(FileConfiguration fileConfiguration) {
+	// createConfigFile();
+	// try {
+	// fileConfiguration.sa;
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
 
-//	private static void createConfigFile() {
-//		if (!configDirectory.exists()) {
-//			configDirectory.mkdir();
-//		}
-//
-//		if (!configFile.exists()) {
-//			try {
-//				configFile.createNewFile();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	}
+	// private static void createConfigFile() {
+	// if (!configDirectory.exists()) {
+	// configDirectory.mkdir();
+	// }
+	//
+	// if (!configFile.exists()) {
+	// try {
+	// configFile.createNewFile();
+	// } catch (IOException e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// }
 
 }
--- src/org/kwstudios/play/ragemode/toolbox/MapChecker.java
+++ src/org/kwstudios/play/ragemode/toolbox/MapChecker.java
@@ -1,37 +1,124 @@
 package org.kwstudios.play.ragemode.toolbox;
 
+import java.util.Set;
+
 import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class MapChecker {
-	
+
 	private static final String GAME_PATH = "settings.games";
-	
+
 	private String gameName;
 	private FileConfiguration fileConfiguration;
 	private boolean isValid = false;
 	String message;
-	
-	public MapChecker(String gameName, FileConfiguration fileConfiguration){
+	int maxPlayers;
+
+	public MapChecker(String gameName, FileConfiguration fileConfiguration) {
 		this.gameName = gameName;
 		this.fileConfiguration = fileConfiguration;
 		checkMapName();
-		checkLobby();
+		if (isValid) {
+			checkBasics();
+		}
+		if (isValid) {
+			checkLobby();
+		}
+		if (isValid) {
+			checkSpawns();
+		}
 	}
-	
-	private void checkMapName(){
-		if(!fileConfiguration.isSet(GAME_PATH + "." + gameName)){
+
+	private void checkMapName() {
+		if (!fileConfiguration.isSet(GAME_PATH + "." + gameName)) {
 			message = ChatColor.YELLOW + gameName + ChatColor.DARK_RED + " is not a valid RageMode Map.";
 			isValid = false;
-		}else{
+		} else {
 			isValid = true;
 		}
 	}
-	
-	private void checkLobby(){
-		if(!fileConfiguration.isSet(GAME_PATH + "." + gameName + "." + "lobby")){
-			message = ChatColor.DARK_RED + "The lobby was not set yet for " + ChatColor.DARK_AQUA + gameName + ". Set it with /rm lobby [game name]";
+
+	private void checkBasics() {
+		String path = GAME_PATH + "." + gameName;
+		if (!fileConfiguration.isSet(path + ".maxplayers") || !fileConfiguration.isSet(path + ".world")) {
+			message = ChatColor.DARK_RED
+					+ "The worldname or the maxplayers are not set. Please contact an Admin for further information.";
+			isValid = false;
+		} else {
+			if (ConfigFactory.getString(path, "world", fileConfiguration) != "") {
+				maxPlayers = ConfigFactory.getInt(path, "maxplayers", fileConfiguration);
+				if (maxPlayers != -32500000) {
+					isValid = true;
+				} else {
+					message = ChatColor.DARK_RED + "The maxplayers value for " + gameName + " is not set properly.";
+					isValid = false;
+				}
+			} else {
+				message = ChatColor.DARK_RED + "The world name can't be empty!";
+				isValid = false;
+			}
 		}
+	}
+
+	private void checkLobby() {
+		if (!fileConfiguration.isSet(GAME_PATH + "." + gameName + "." + "lobby")) {
+			message = ChatColor.DARK_RED + "The lobby was not set yet for " + ChatColor.DARK_AQUA + gameName
+					+ ". Set it with /rm lobby [game name]";
+			isValid = false;
+		} else {
+			String thisPath = GAME_PATH + "." + gameName + "." + "lobby";
+			if (fileConfiguration.isSet(thisPath + ".x") && fileConfiguration.isSet(thisPath + ".y")
+					&& fileConfiguration.isSet(thisPath + ".z") && fileConfiguration.isSet(thisPath + ".world")) {
+				if (ConfigFactory.getString(thisPath, "world", fileConfiguration) != "") {
+					if (ConfigFactory.getInt(thisPath, "x", fileConfiguration) != -32500000
+							&& ConfigFactory.getInt(thisPath, "y", fileConfiguration) != -32500000
+							&& ConfigFactory.getInt(thisPath, "z", fileConfiguration) != -32500000) {
+						isValid = true;
+					} else {
+						message = ChatColor.DARK_RED
+								+ "The lobby coordinates were not set properly. Ask an Admin to check the config.yml";
+						isValid = false;
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	private void checkSpawns() {
+		String path = GAME_PATH + "." + gameName;
+		if (fileConfiguration.isSet(path + ".spawns")) {
+			Set<String> spawnNames = ConfigFactory.getKeysUnderPath(path + ".spawns", false, fileConfiguration);
+			if (spawnNames.size() >= maxPlayers) {
+				for (String s : spawnNames) {
+					if (ConfigFactory.getInt(path + ".spawns." + s, "x", fileConfiguration) != -32500000
+							&& ConfigFactory.getInt(path + ".spawns." + s, "y", fileConfiguration) != -32500000
+							&& ConfigFactory.getInt(path + ".spawns." + s, "z", fileConfiguration) != -32500000) {
+						isValid = true;
+					} else {
+						message = ChatColor.DARK_RED + "One or more spawns are not set properly!";
+						isValid = false;
+						break;
+					}
+				}
+			} else {
+				message = ChatColor.DARK_RED
+						+ "The number of spawns must be greater than or equal the maxplayers value!";
+				isValid = false;
+			}
+		} else {
+			message = ChatColor.DARK_RED + "In " + gameName + " are no spawns configured!";
+			isValid = false;
+		}
+	}
+
+	public boolean isValid() {
+		return isValid;
+	}
+
+	public String getMessage() {
+		return message;
 	}
 
 }

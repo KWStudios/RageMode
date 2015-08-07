6f6987d813b4960e329ead79bac1cd8b542c6d96
Y-brin <koray@koska.at>
2015-08-07 21:15:30 +0200

    Changed RageScores.

--- src/org/kwstudios/play/ragemode/gameLogic/RageScores.java
+++ src/org/kwstudios/play/ragemode/gameLogic/RageScores.java
@@ -23,8 +23,8 @@ public class RageScores {
 					+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with a direct arrow hit. "
 					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + bowPoints);
 
-			victim.sendMessage(
-					ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by " + killer.getName());
+			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
+					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
 			break;
 		case "combataxe":
 			int axePoints = ConstantHolder.POINTS_FOR_AXE_KILL;
@@ -35,7 +35,8 @@ public class RageScores {
 					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + axePoints);
 
 			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
-					+ killer.getName() + ChatColor.BOLD.toString() + ChatColor.DARK_RED.toString() + " -" + axePoints);
+					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName()
+					+ ChatColor.BOLD.toString() + ChatColor.DARK_RED.toString() + " -" + axePoints);
 			break;
 		case "rageknife":
 			int knifePoints = ConstantHolder.POINTS_FOR_KNIFE_KILL;
@@ -44,9 +45,9 @@ public class RageScores {
 					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + victim.getName()
 					+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with your RageKnife. "
 					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + knifePoints);
-			
-			victim.sendMessage(
-					ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by " + killer.getName());
+
+			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
+					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
 			break;
 		case "explosion":
 			int explosionPoints = ConstantHolder.POINTS_FOR_EXPLOSION_KILL;
@@ -56,9 +57,9 @@ public class RageScores {
 							+ ChatColor.BOLD.toString() + victim.getName() + ChatColor.RESET.toString()
 							+ ChatColor.DARK_AQUA + " by causing heavy explosions with your RageBow. "
 							+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + explosionPoints);
-			
-			victim.sendMessage(
-					ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by " + killer.getName());
+
+			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
+					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
 			break;
 		default:
 			break;

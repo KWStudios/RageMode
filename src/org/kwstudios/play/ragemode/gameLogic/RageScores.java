1afbb5cd23ec2db09c1e3158fc33d38703f83dc3
Y-brin <koray@koska.at>
2015-08-07 23:42:59 +0200

    Removed suicide points.

--- src/org/kwstudios/play/ragemode/gameLogic/RageScores.java
+++ src/org/kwstudios/play/ragemode/gameLogic/RageScores.java
@@ -14,71 +14,76 @@ public class RageScores {
 	private static int totalPoints = 0;
 
 	public static void addPointsToPlayer(Player killer, Player victim, String killCause) {
-		switch (killCause.toLowerCase()) {
-		case "ragebow":
-			int bowPoints = ConstantHolder.POINTS_FOR_BOW_KILL;
-			totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), bowPoints);
-			killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed "
-					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + victim.getName()
-					+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with a direct arrow hit. "
-					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + bowPoints);
+		if (!killer.getUniqueId().toString().equals(victim.getUniqueId().toString())) {
+			switch (killCause.toLowerCase()) {
+			case "ragebow":
+				int bowPoints = ConstantHolder.POINTS_FOR_BOW_KILL;
+				totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), bowPoints);
+				killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed "
+						+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + victim.getName()
+						+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with a direct arrow hit. "
+						+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + bowPoints);
 
-			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
+				victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
 
-			killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints + ChatColor.RESET.toString()
-					+ ChatColor.DARK_AQUA.toString() + " points.");
-			break;
-		case "combataxe":
-			int axePoints = ConstantHolder.POINTS_FOR_AXE_KILL;
-			totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), axePoints);
-			killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed "
-					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + victim.getName()
-					+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with your CombatAxe. "
-					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + axePoints);
+				killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints
+						+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA.toString() + " points.");
+				break;
+			case "combataxe":
+				int axePoints = ConstantHolder.POINTS_FOR_AXE_KILL;
+				totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), axePoints);
+				killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed "
+						+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + victim.getName()
+						+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with your CombatAxe. "
+						+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + axePoints);
 
-			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName()
-					+ ChatColor.BOLD.toString() + ChatColor.DARK_RED.toString() + " -" + axePoints);
+				victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName()
+						+ ChatColor.BOLD.toString() + ChatColor.DARK_RED.toString() + " -" + axePoints);
 
-			killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints + ChatColor.RESET.toString()
-					+ ChatColor.DARK_AQUA.toString() + " points.");
-			break;
-		case "rageknife":
-			int knifePoints = ConstantHolder.POINTS_FOR_KNIFE_KILL;
-			totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), knifePoints);
-			killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed "
-					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + victim.getName()
-					+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with your RageKnife. "
-					+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + knifePoints);
+				killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints
+						+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA.toString() + " points.");
+				break;
+			case "rageknife":
+				int knifePoints = ConstantHolder.POINTS_FOR_KNIFE_KILL;
+				totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), knifePoints);
+				killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed "
+						+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + victim.getName()
+						+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA + " with your RageKnife. "
+						+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + knifePoints);
 
-			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
+				victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
 
-			killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints + ChatColor.RESET.toString()
-					+ ChatColor.DARK_AQUA.toString() + " points.");
-			break;
-		case "explosion":
-			int explosionPoints = ConstantHolder.POINTS_FOR_EXPLOSION_KILL;
-			totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), explosionPoints);
+				killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints
+						+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA.toString() + " points.");
+				break;
+			case "explosion":
+				int explosionPoints = ConstantHolder.POINTS_FOR_EXPLOSION_KILL;
+				totalPoints = addPoints(killer, PlayerList.getPlayersGame(killer), explosionPoints);
+				killer.sendMessage(
+						ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed " + ChatColor.GOLD.toString()
+								+ ChatColor.BOLD.toString() + victim.getName() + ChatColor.RESET.toString()
+								+ ChatColor.DARK_AQUA + " by causing heavy explosions with your RageBow. "
+								+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + explosionPoints);
+
+				victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
+
+				killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
+						+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints
+						+ ChatColor.RESET.toString() + ChatColor.DARK_AQUA.toString() + " points.");
+				break;
+			default:
+				break;
+			}
+		} else {
 			killer.sendMessage(
-					ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed " + ChatColor.GOLD.toString()
-							+ ChatColor.BOLD.toString() + victim.getName() + ChatColor.RESET.toString()
-							+ ChatColor.DARK_AQUA + " by causing heavy explosions with your RageBow. "
-							+ ChatColor.GOLD.toString() + ChatColor.BOLD.toString() + "+" + explosionPoints);
-
-			victim.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You were killed by "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + killer.getName());
-
-			killer.sendMessage(ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You now have "
-					+ ChatColor.BOLD.toString() + ChatColor.GOLD.toString() + totalPoints + ChatColor.RESET.toString()
-					+ ChatColor.DARK_AQUA.toString() + " points.");
-			break;
-		default:
-			break;
+					ConstantHolder.RAGEMODE_PREFIX + ChatColor.DARK_AQUA + "You killed yourself you silly idiot.");
 		}
 	}
 

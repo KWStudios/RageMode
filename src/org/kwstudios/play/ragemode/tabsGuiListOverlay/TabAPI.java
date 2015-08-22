363d8b48b83c5359b691f246b4f3bed09c61ab02
Simon <simon.artner@gmail.com>
2015-08-23 00:00:03 +0200

    Packets

--- src/org/kwstudios/play/ragemode/tabsGuiListOverlay/TabAPI.java
+++ src/org/kwstudios/play/ragemode/tabsGuiListOverlay/TabAPI.java
@@ -9,11 +9,15 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
+import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo;
+import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer;
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo.EnumPlayerInfoAction;
+import net.minecraft.server.v1_8_R3.WorldSettings.EnumGamemode;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import com.comphenix.protocol.PacketType;
@@ -44,30 +48,48 @@ public class TabAPI {
 
 //		PlayerInfoData gre = new PlayerInfoData(new WrappedGameProfile(UUID.randomUUID(), "Stuhl"), 1, Bukkit.getPlayer(UUID.fromString(players.get(0))).getGameMode(), new WrappedChatComponent());
 //		new Player
-		PacketContainer playerRemovefromTab = protocolManager.createPacket(PacketType.Play.Server.PLAYER_INFO);
+	/*	PacketContainer playerRemovefromTab = protocolManager.createPacket(PacketType.Play.Server.PLAYER_INFO);
 
-		playerRemovefromTab.getModifier().write(0, EnumPlayerInfoAction.REMOVE_PLAYER);
-		
-		int i = 0;
+		playerRemovefromTab.getModifier().write(0, EnumPlayerInfoAction.REMOVE_PLAYER);*/
 		Collection<Player> op = (Collection<Player>) Bukkit.getOnlinePlayers();
-		int imax = op.size();
 		Player[] oP = new Player[1];
 		op.toArray(oP);
-		byte[] baUuid = new byte[16];
-		List hhu = new ArrayList();
-		while(i < imax) {
-			
-			PacketPlayOutPlayerInfo.PlayerInfoData data = new PacketPlayOutPlayerInfo.PlayerInfoData(new GameProfile(oP[i].getUniqueId(), ""), 2, oP[i].getGameMode(), null);
-			PacketPlayOutPlayerInfo.PlayerInfoData data = new PacketPlayOutPlayerInfo.PlayerInfoData()
-			i++;
-		}
-		playerRemovefromTab.getModifier().write(1, data);
+		
+		PacketPlayOutPlayerInfo packet = null;
+		
+/*		byte[] baUuid = new byte[16];
+		
+		List<PlayerInfoData> hhu = new ArrayList<PlayerInfoData>();
+		PlayerInfoData data;*/
+
+/*		playerRemovefromTab.getModifier().write(1, hhu);
 		try {
 			protocolManager.sendServerPacket(Bukkit.getPlayer(UUID.fromString(players.get(0))), playerRemovefromTab);
 		} catch (InvocationTargetException e) {
 			e.printStackTrace();
-		}
+		}*/
 //		playerRemovefromTab.getS
+		int i = 0;
+		int imax = op.size();
+		
+		int n = 0;
+		int nmax = players.size();
+		Bukkit.broadcastMessage("Starting removal: " + nmax + imax);
+		while(n < nmax) {
+			while(i < imax) {/*			
+				data = new PlayerInfoData(new GameProfile(oP[i].getUniqueId(), ""), 2, NativeGamemode.CREATIVE, ChatSerializer.a("Something?"));
+				new PlayerInfoData()
+				hhu.add(data);
+				*/
+				Bukkit.broadcastMessage("Removed: " + oP[i].getName());
+				packet = new PacketPlayOutPlayerInfo(EnumPlayerInfoAction.REMOVE_PLAYER, ((CraftPlayer)oP[i]).getHandle());
+				
+				Bukkit.broadcastMessage("for Player: " + Bukkit.getPlayer(UUID.fromString(players.get(0))).getName());
+				((CraftPlayer)Bukkit.getPlayer(UUID.fromString(players.get(n)))).getHandle().playerConnection.sendPacket(packet);
+				i++;
+			}
+			n++;
+		}
 	}
 	
 	public static void updateTabGuiListOverlayForGame(String game) {

4e2ad61875ffe73263a1120e92ccc5c6c03ba80d
Ybrin <koray@koska.at>
2015-08-31 23:33:50 +0200

    Added functions to MySQLStats.

--- src/org/kwstudios/play/ragemode/statistics/MySQLStats.java
+++ src/org/kwstudios/play/ragemode/statistics/MySQLStats.java
@@ -5,11 +5,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.bukkit.entity.Player;
 import org.kwstudios.play.ragemode.database.MySQLConnector;
 import org.kwstudios.play.ragemode.gameLogic.PlayerPoints;
+import org.kwstudios.play.ragemode.gameLogic.RetPlayerPoints;
 
 public class MySQLStats {
 
+	/**
+	 * Adds the statistics from the given PlayerPoints instance to the database
+	 * connection from the given MySQLConnector instance.
+	 * 
+	 * @param playerPoints
+	 *            The PlayerPoints instance from which the statistics should be
+	 *            gotten.
+	 * @param mySQLConnector
+	 *            The MySQLConnector instance which holds the Connection for the
+	 *            database.
+	 */
 	public static void addPlayerStatistics(PlayerPoints playerPoints, MySQLConnector mySQLConnector) {
 		Connection connection = mySQLConnector.getConnection();
 
@@ -21,11 +34,13 @@ public class MySQLStats {
 		int oldDirectArrowKills = 0;
 		int oldExplosionKills = 0;
 		int oldKnifeKills = 0;
+
 		int oldDeaths = 0;
 		int oldAxeDeaths = 0;
 		int oldDirectArrowDeaths = 0;
 		int oldExplosionDeaths = 0;
 		int oldKnifeDeaths = 0;
+
 		int oldWins = 0;
 		int oldScore = 0;
 		int oldGames = 0;
@@ -39,11 +54,13 @@ public class MySQLStats {
 				oldDirectArrowKills = rs.getInt("direct_arrow_kills");
 				oldExplosionKills = rs.getInt("explosion_kills");
 				oldKnifeKills = rs.getInt("knife_kills");
+
 				oldDeaths = rs.getInt("deaths");
 				oldAxeDeaths = rs.getInt("axe_deaths");
 				oldDirectArrowDeaths = rs.getInt("direct_arrow_deaths");
 				oldExplosionDeaths = rs.getInt("explosion_deaths");
 				oldKnifeDeaths = rs.getInt("knife_deaths");
+
 				oldWins = rs.getInt("wins");
 				oldScore = rs.getInt("score");
 				oldGames = rs.getInt("games");
@@ -65,11 +82,13 @@ public class MySQLStats {
 		int newDirectArrowKills = oldDirectArrowKills + playerPoints.getDirectArrowKills();
 		int newExplosionKills = oldExplosionKills + playerPoints.getExplosionKills();
 		int newKnifeKills = oldKnifeKills + playerPoints.getKnifeKills();
+
 		int newDeaths = oldDeaths + playerPoints.getDeaths();
 		int newAxeDeaths = oldAxeDeaths + playerPoints.getAxeDeaths();
 		int newDirectArrowDeaths = oldDirectArrowDeaths + playerPoints.getDirectArrowDeaths();
 		int newExplosionDeaths = oldExplosionDeaths + playerPoints.getExplosionDeaths();
 		int newKnifeDeaths = oldKnifeDeaths + playerPoints.getKnifeDeaths();
+
 		int newWins = (playerPoints.isWinner()) ? oldWins++ : oldWins;
 		int newScore = oldScore + playerPoints.getPoints();
 		int newGames = oldGames++;
@@ -91,6 +110,93 @@ public class MySQLStats {
 			e.printStackTrace();
 		}
 
+	}
+
+	/**
+	 * Returns an RetPlayerPoints instance with the statistics from the database
+	 * connection for the given Player.
+	 * 
+	 * @param player
+	 *            The Player instance for which the statistic should be gotten.
+	 * @param mySQLConnector
+	 *            The MySQLConnector which holds the Connection instance for the
+	 *            database.
+	 * @return
+	 */
+	public static RetPlayerPoints getPlayerStatistics(Player player, MySQLConnector mySQLConnector) {
+		Connection connection = mySQLConnector.getConnection();
+
+		Statement statement = null;
+		String query = "SELECT * FROM rm_stats_players WHERE uuid LIKE " + player.getUniqueId().toString() + ";";
+
+		int currentKills = 0;
+		int currentAxeKills = 0;
+		int currentDirectArrowKills = 0;
+		int currentExplosionKills = 0;
+		int currentKnifeKills = 0;
+
+		int currentDeaths = 0;
+		int currentAxeDeaths = 0;
+		int currentDirectArrowDeaths = 0;
+		int currentExplosionDeaths = 0;
+		int currentKnifeDeaths = 0;
+
+		int currentWins = 0;
+		int currentScore = 0;
+		int currentGames = 0;
+		double currentKD = 0;
+
+		try {
+			statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(query);
+			while (rs.next()) {
+				currentKills = rs.getInt("kills");
+				currentAxeKills = rs.getInt("axe_kills");
+				currentDirectArrowKills = rs.getInt("direct_arrow_kills");
+				currentExplosionKills = rs.getInt("explosion_kills");
+				currentKnifeKills = rs.getInt("knife_kills");
+
+				currentDeaths = rs.getInt("deaths");
+				currentAxeDeaths = rs.getInt("axe_deaths");
+				currentDirectArrowDeaths = rs.getInt("direct_arrow_deaths");
+				currentExplosionDeaths = rs.getInt("explosion_deaths");
+				currentKnifeDeaths = rs.getInt("knife_deaths");
+
+				currentWins = rs.getInt("wins");
+				currentScore = rs.getInt("score");
+				currentGames = rs.getInt("games");
+				currentKD = rs.getDouble("kd");
+			}
+		} catch (SQLException e) {
+			System.out.println("Something went wrong!");
+			return null;
+		}
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		RetPlayerPoints retPlayerPoints = new RetPlayerPoints(player.getUniqueId().toString());
+		retPlayerPoints.setKills(currentKills);
+		retPlayerPoints.setAxeKills(currentAxeKills);
+		retPlayerPoints.setDirectArrowKills(currentDirectArrowKills);
+		retPlayerPoints.setExplosionKills(currentExplosionKills);
+		retPlayerPoints.setKnifeKills(currentKnifeKills);
+
+		retPlayerPoints.setDeaths(currentDeaths);
+		retPlayerPoints.setAxeDeaths(currentAxeDeaths);
+		retPlayerPoints.setDirectArrowDeaths(currentDirectArrowDeaths);
+		retPlayerPoints.setExplosionDeaths(currentExplosionDeaths);
+		retPlayerPoints.setKnifeDeaths(currentKnifeDeaths);
+
+		retPlayerPoints.setWins(currentWins);
+		retPlayerPoints.setPoints(currentScore);
+		retPlayerPoints.setGames(currentGames);
+		retPlayerPoints.setKD(currentKD);
+
+		return retPlayerPoints;
 	}
 
 }
